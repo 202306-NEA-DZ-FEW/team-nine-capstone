0e716420876a48e46decf6aa770d1bf643b31705
HachemBouhadede <hachembouhadede@gmail.com>
2023-10-14 10:26:38 +0200

    feat(layout: navbar responsivness): navbar fully responsivness with all devices,+ a remaining bug
    full responviness with all devices, hamburger menu availble for phone, drop down styling adjust for
    all media
    
    feat #3

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,97 +1,185 @@
 import Link from "next/link";
 import React, { useState } from "react";
-import { CgProfile } from "react-icons/cg";
+import { CgClose, CgProfile } from "react-icons/cg";
+import { GiHamburgerMenu } from "react-icons/gi";
 import { LiaLanguageSolid } from "react-icons/lia";
 
 const Navbar = () => {
     const [user, setUser] = useState(true); // Set to true if a user is authenticated, otherwise, set it to false
     const [isProfileDropdownOpen, setProfileDropdownOpen] = useState(false);
     const [isLanguageDropdownOpen, setLanguageDropdownOpen] = useState(false);
+    const [isHamburgerOn, setIsHamburgerOn] = useState(false);
 
     const toggleProfileDropdown = () => {
         setProfileDropdownOpen(!isProfileDropdownOpen);
+        setLanguageDropdownOpen(false);
     };
 
     const toggleLanguageDropdown = () => {
         setLanguageDropdownOpen(!isLanguageDropdownOpen);
+        setProfileDropdownOpen(false);
     };
 
     const closeDropdowns = () => {
-        if (isProfileDropdownOpen) setProfileDropdownOpen(false);
-        if (isLanguageDropdownOpen) setLanguageDropdownOpen(false);
+        setProfileDropdownOpen(false);
+        setLanguageDropdownOpen(false);
+    };
+
+    const toggleHamburger = () => {
+        setIsHamburgerOn(!isHamburgerOn);
     };
 
     return (
-        <nav className='w-screen sticky top-0 layout p-4 text-white'>
-            <div className='flex justify-between items-center'>
-                <div>
-                    <Link href='/'>
-                        <div className='text-xl font-bold'>LOGO</div>
-                    </Link>
-                </div>
-                <div className='flex items-center space-x-4'>
+        <nav className='layout sticky w-screen items-center flex justify-between p-2'>
+            <div className='p-1 font-bold'> Logo </div>
+
+            {/* Mobile view */}
+            <div
+                className='absolute right-1 lg:hidden p-2 flex-col justify-between hover:bg-amber-300 hover:text-white rounded-lg'
+                onClick={toggleHamburger}
+            >
+                {isHamburgerOn ? <CgClose /> : <GiHamburgerMenu />}
+                {isHamburgerOn && (
+                    <div className='fixed top-12 left-0 w-full layout z-50'>
+                        <div className='w-full items-center flex flex-col'>
+                            <button
+                                className='w-full p-2 flex items-center justify-center hover:bg-amber-300 hover:text-white rounded-lg'
+                                onClick={toggleProfileDropdown}
+                            >
+                                <CgProfile /> {user ? "Profile" : "SignUp"}
+                            </button>
+                            {isProfileDropdownOpen && (
+                                <div className='w-full'>
+                                    {user ? (
+                                        <>
+                                            <Link href='/profile'>
+                                                <button className='w-full p-2 flex items-center justify-center hover:bg-amber-300 hover:text-white rounded-lg'>
+                                                    Your Profile
+                                                </button>
+                                            </Link>
+                                            <Link href='/events'>
+                                                <button className='w-full p-2 flex items-center justify-center hover:bg-amber-300 hover:text-white rounded-lg'>
+                                                    Your Events
+                                                </button>
+                                            </Link>
+                                            <button className='w-full p-2 flex items-center justify-center hover:bg-amber-300 hover:text-white rounded-lg'>
+                                                Logout
+                                            </button>
+                                        </>
+                                    ) : (
+                                        <>
+                                            <Link href='/signin'>
+                                                <button className='w-full p-2 flex items-center justify-center hover:bg-amber-300 hover:text-white rounded-lg'>
+                                                    Sign In
+                                                </button>
+                                            </Link>
+                                            <Link href='/signup'>
+                                                <button className='w-full p-2 flex items-center justify-center hover:bg-amber-300 hover:text-white rounded-lg'>
+                                                    Sign Up
+                                                </button>
+                                            </Link>
+                                        </>
+                                    )}
+                                </div>
+                            )}
+                            <button
+                                className='w-full p-2 flex items-center justify-center hover:bg-amber-300 hover:text-white rounded-lg'
+                                onClick={toggleLanguageDropdown}
+                            >
+                                <LiaLanguageSolid /> Language
+                            </button>
+                            {isLanguageDropdownOpen && (
+                                <div className='w-full'>
+                                    <button className='w-full p-2 flex items-center justify-center hover:bg-amber-300 hover:text-white rounded-lg'>
+                                        English
+                                    </button>
+                                    <button className='w-full p-2 flex items-center justify-center hover:bg-amber-300 hover:text-white rounded-lg'>
+                                        العربية
+                                    </button>
+                                </div>
+                            )}
+                            <button className='w-full p-2 flex items-center justify-center hover:bg-amber-300 hover:text-white rounded-lg'>
+                                <Link href='#'>About</Link>
+                            </button>
+                        </div>
+                    </div>
+                )}
+            </div>
+
+            {/* Desktop view */}
+            <div className='hidden lg:flex flex-row-reverse text-center '>
+                <div className='relative'>
+                    {/* Profile Dropdown */}
                     <button
-                        className='relative hover:bg-white hover:text-black rounded-full p-2'
-                        onClick={toggleLanguageDropdown}
-                        onBlur={closeDropdowns}
-                    >
-                        <LiaLanguageSolid className='text-2xl' />
-                        {isLanguageDropdownOpen && (
-                            <div className='absolute top-10 right-0 mt-2 layout w-32 p-2'>
-                                <button className='block w-full text-left p-2  hover:bg-amber-300 hover:rounded-lg'>
-                                    English
-                                </button>
-                                <button className='block w-full text-left p-2  hover:bg-amber-300 hover:rounded-lg'>
-                                    العربية
-                                </button>
-                            </div>
-                        )}
-                    </button>
-                    <button
-                        className='relative hover:bg-white hover:text-black rounded-full p-2'
+                        className='p-2 mx-2 flex items-center hover:bg-amber-300 hover:text-white rounded-lg'
                         onClick={toggleProfileDropdown}
                         onBlur={closeDropdowns}
                     >
-                        <CgProfile className='text-2xl' />
-                        {isProfileDropdownOpen && (
-                            <div className='absolute top-10 right-0 mt-2 layout w-32 p-2'>
-                                {user ? (
-                                    <>
-                                        {/* first case if the profile is clicked and there's a user */}
-                                        <Link href='/' /*"/profile"*/>
-                                            <div className='block w-full text-left p-2  hover:bg-amber-300 hover:rounded-lg'>
-                                                Your Profile
-                                            </div>
-                                        </Link>
-                                        <Link href='/' /*"/events"*/>
-                                            <div className='block w-full text-left p-2  hover:bg-amber-300 hover:rounded-lg'>
-                                                Your Events
-                                            </div>
-                                        </Link>
-                                        <button
-                                            className='block w-full text-left p-2  hover:bg-amber-300 hover:rounded-lg' /*onClick={signOut}*/
-                                        >
-                                            Logout
-                                        </button>
-                                    </>
-                                ) : (
-                                    <>
-                                        <Link href='/' /*"/signin"*/>
-                                            <div className='block w-full text-left p-2  hover:bg-amber-300 hover:rounded-lg'>
-                                                Sign In
-                                            </div>
-                                        </Link>
-                                        <Link href='/' /*"/signup"*/>
-                                            <div className='block w-full text-left p-2  hover:bg-amber-300 hover:rounded-lg'>
-                                                Sign Up
-                                            </div>
-                                        </Link>
-                                    </>
-                                )}
-                            </div>
-                        )}
+                        <CgProfile /> {user ? "Profile" : "Sign Up"}
                     </button>
+                    {isProfileDropdownOpen && (
+                        <div className='absolute layout top-full mt-2 w-28 p-2 border border-gray-400 rounded-lg'>
+                            {user ? (
+                                <>
+                                    <Link href='/' /*"/profile"*/>
+                                        <div className='block w-full text-left p-2  hover:bg-amber-300 hover:rounded-lg'>
+                                            Your Profile
+                                        </div>
+                                    </Link>
+                                    <Link href='/' /*"/events"*/>
+                                        <div className='block w-full text-left p-2  hover:bg-amber-300 hover:rounded-lg'>
+                                            Your Events
+                                        </div>
+                                    </Link>
+                                    <button
+                                        className='block w-full text-left p-2  hover:bg-amber-300 hover:rounded-lg' /*onClick={signOut}*/
+                                    >
+                                        Logout
+                                    </button>
+                                </>
+                            ) : (
+                                <>
+                                    <Link href='/' /*"/signin"*/>
+                                        <div className='block w-full text-left p-2  hover:bg-amber-300 hover:rounded-lg'>
+                                            Sign In
+                                        </div>
+                                    </Link>
+                                    <Link href='/' /*"/signup"*/>
+                                        <div className='block w-full text-left p-2  hover:bg-amber-300 hover:rounded-lg'>
+                                            Sign Up
+                                        </div>
+                                    </Link>
+                                </>
+                            )}
+                        </div>
+                    )}
                 </div>
+                <div className='relative'>
+                    {/* Language Dropdown */}
+                    <button
+                        className='p-2 mx-2 flex items-center hover:bg-amber-300 hover:text-white rounded-lg'
+                        onClick={toggleLanguageDropdown}
+                        onBlur={closeDropdowns}
+                    >
+                        <LiaLanguageSolid /> Language
+                    </button>
+                    {isLanguageDropdownOpen && (
+                        <div className='absolute top-full layout left-1 mt-2 w-32 p-2 border border-gray-400 rounded-lg'>
+                            <ul>
+                                <li className='block w-full text-left p-2  hover:bg-amber-300 hover:rounded-lg'>
+                                    English
+                                </li>
+                                <li className='block w-full text-left p-2  hover:bg-amber-300 hover:rounded-lg'>
+                                    العربية
+                                </li>
+                            </ul>
+                        </div>
+                    )}
+                </div>
+                <button className='p-2 mx-2 hover:bg-amber-300 hover:text-white rounded-lg'>
+                    {" "}
+                    <Link href='#'>About</Link>
+                </button>
             </div>
         </nav>
     );
