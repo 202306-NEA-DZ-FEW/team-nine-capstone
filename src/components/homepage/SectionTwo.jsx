172c0f3a94ea17be5254b8503a564699c72c3182
HachemBouhadede <hachembouhadede@gmail.com>
2023-10-15 23:22:15 +0200

    build(fix path): componenets dublicate removed

---
+++ src/components/homepage/SectionTwo.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+
+const SectionTwo = () => {
+    return (
+        <div className='container mx-auto flex  flex-col justify-center'>
+            <div>
+                <div className='flex flex-col justify-center items-center'>
+                    <h1>Sustainable Developments Goals</h1>
+                    <p>
+                        Lorem, ipsum dolor sit amet consectetur adipisicing
+                        elit. Nisi voluptas, quam nulla eveniet esse dolorem!
+                        Aspernatur minus tenetur corporis perferendis?
+                    </p>
+                </div>
+                <div></div>
+            </div>
+            <div></div>
+            <div></div>
+        </div>
+    );
+};
+
+export default SectionTwo;
