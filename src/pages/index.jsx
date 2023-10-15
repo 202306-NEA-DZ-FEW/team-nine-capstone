8f7ccd9691c6926cfc60d92406ea336b6bad410f
oualidelh <elhouarioualid@gmail.com>
2023-10-15 20:54:45 +0100

    feat(new feat): adding small features and some styling
    feat #8

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -3,6 +3,7 @@ import { useTranslation } from "next-i18next";
 import { serverSideTranslations } from "next-i18next/serverSideTranslations";
 import * as React from "react";
 
+import SectionTwo from "@/componants/homepagecomponats/SectionTwo";
 import Layout from "@/layout/Layout";
 
 export default function HomePage() {
@@ -19,6 +20,7 @@ export default function HomePage() {
                     العربية
                 </Link>
             </div>
+            <SectionTwo />
         </Layout>
     );
 }
