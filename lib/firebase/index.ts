b99285ea9a14e0beac0767f727d5df0c4eed870e
Khalil NOUI <133600682+Khalil-NOUI@users.noreply.github.com>
2023-10-16 06:20:52 +0100

    Update index.ts

--- lib/firebase/index.ts
+++ lib/firebase/index.ts
@@ -3,12 +3,6 @@ import { initializeApp } from "firebase/app";
 import {getAuth} from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDIey9kE6GGfQfZrkFn6qdsVMsZt_QwhEw",
-  authDomain: "pebble-d419a.firebaseapp.com",
-  projectId: "pebble-d419a",
-  storageBucket: "pebble-d419a.appspot.com",
-  messagingSenderId: "458766123239",
-  appId: "1:458766123239:web:5a9791e15c6188e338a242"
 };
 
 
