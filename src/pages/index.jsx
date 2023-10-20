d4dc84edccb64516636769f29fc88a612398b870
iman-om <imane3.omari@gmail.com>
2023-10-20 15:26:47 +0100

    fix(herosection.jsx and howitworks.jsx): fix responsiveness
    fix responsiveness and images not showing up

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -3,8 +3,9 @@ import { useTranslation } from "next-i18next";
 import { serverSideTranslations } from "next-i18next/serverSideTranslations";
 import * as React from "react";
 
-import HeroSection from "@/components/HomePage/HeroSection";
-import HowitWorks from "@/Components/homepage/HowitWorks";
+import HeroSection from "@/components/homepage/HeroSection";
+import HowitWorks from "@/components/homepage/HowitWorks";
+
 import Layout from "@/layout/Layout";
 
 export default function HomePage() {
