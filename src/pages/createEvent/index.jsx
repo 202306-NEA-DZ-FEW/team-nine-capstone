78d0711d28d3823c2f6ff2ea27cbb464c680e401
Khalil-NOUI <NOUI.KHALIL.PR@GMAIL.COM>
2023-10-23 02:50:43 +0100

    fix(fix edeploy errrors): update import
    import necessary modules
    
    BREAKING CHANGE: enable deployment
    
    #23

--- src/pages/createEvent/index.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-import { CreateEvent } from "../../components/createEventPageComponents/CreateEvent";
-
-function CreatEvent() {
-    return <CreateEvent />;
-}
-
-export default CreatEvent;
