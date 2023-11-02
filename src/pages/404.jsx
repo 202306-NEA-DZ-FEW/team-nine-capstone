2cf0f3699b66e9af92c3720b4d5fa886c13fa945
Khalil-NOUI <NOUI.KHALIL.PR@GMAIL.COM>
2023-11-02 16:15:49 +0100

    fix(yourevents, yourevents): filter users own collection
    enable user to filter and search through his/her own collection of events according to intersest,
    date, and location
    
    BREAKING CHANGE: filter events of user
    
    resolve #53

--- src/pages/404.jsx
+++ src/pages/404.jsx
@@ -1,7 +1,32 @@
-import * as React from "react";
+import Link from "next/link";
 
 import Layout from "@/layout/Layout";
 
-export default function NotFoundPage() {
-    return <Layout>404</Layout>;
-}
+const Error404Page = () => (
+    <Layout>
+        <div className='error-container'>
+            <h1>404 - Page Not Found</h1>
+            <p>Oops! The page you&apos;re looking for doesn&apos;t exist.</p>
+            <Link href='/'>Go back to the home page</Link>
+
+            <style jsx>{`
+                .error-container {
+                    text-align: center;
+                    padding: 2rem;
+                }
+                h1 {
+                    font-size: 2.5rem;
+                }
+                a {
+                    color: #0070f3;
+                    text-decoration: none;
+                }
+                a:hover {
+                    text-decoration: underline;
+                }
+            `}</style>
+        </div>
+    </Layout>
+);
+
+export default Error404Page;
--- src/pages/events/[id].jsx
+++ src/pages/events/[id].jsx
@@ -2,8 +2,14 @@ import React from "react";
 
 import EventDetails from "@/components/singlEventPageComponents/EventDetails";
 
+import Layout from "../../layout/Layout";
+
 function singleEventPage() {
-    return <EventDetails />;
+    return (
+        <Layout>
+            <EventDetails />
+        </Layout>
+    );
 }
 
 export default singleEventPage;
