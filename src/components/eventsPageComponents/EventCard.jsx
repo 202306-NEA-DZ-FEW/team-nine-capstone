f848b789dc7008935b88bbff150f756e72350e37
Hachem "Timeless" Bouhadede <137968168+HachemBouhadede@users.noreply.github.com>
2023-11-05 00:08:10 +0100

    Revert "feat(add new features): add the number of attendees in the event"

--- src/components/eventsPageComponents/EventCard.jsx
+++ src/components/eventsPageComponents/EventCard.jsx
@@ -1,50 +1,16 @@
-import { doc, updateDoc } from "firebase/firestore";
 import Link from "next/link";
-import { useRouter } from "next/router";
-import { useTranslation } from "next-i18next";
 import React from "react";
-import { FaPeopleGroup } from "react-icons/fa6";
-
-import { eventsCollection } from "@/lib/firebase/controller";
 
 import { useUser } from "@/context/UserContext";
 
 function EventCard({ TheEvent }) {
     const { user } = useUser();
-    const router = useRouter();
-    const docRef = doc(eventsCollection, TheEvent.id);
-    // var to handle translatons
-    const { t } = useTranslation();
-    // method to handle attendees
-    const handleAttendees = () => {
-        if (user) {
-            // Check if 'TheEvent.attendees' is an array, and if not, initialize it as an empty array
-            const attendees = Array.isArray(TheEvent.attendees)
-                ? TheEvent.attendees
-                : [];
 
-            // Check if the user's ID is already in the 'attendees' array
-            const userIndex = attendees.indexOf(user.uid);
+    // console.log("TheEvent structure", TheEvent);
+    // console.log(TheEvent.data);
+    // const { user } = useUser();
+    // console.log(user)
 
-            if (userIndex !== -1) {
-                // If the user's ID is already in the array, remove it
-                attendees.splice(userIndex, 1);
-            } else {
-                // If the user's ID is not in the array, add it
-                attendees.push(user.uid);
-            }
-
-            // Create the updated event object with the modified 'attendees' array
-            const updatedTheEvent = {
-                attendees,
-            };
-
-            // Update the event document with the new 'updatedTheEvent' data
-            updateDoc(docRef, updatedTheEvent);
-        } else {
-            router.push("/authentication/signUp");
-        }
-    };
     return (
         <div className='mx-auto bg-gray-700 flex items-center justify-center px-8 py-8'>
             <div className='flex flex-col w-full bg-white rounded shadow-lg sm:w-3/4 md:w-1/2 lg:w-3/5'>
@@ -82,36 +48,6 @@ function EventCard({ TheEvent }) {
                                     height={100}
                                     className='w-8'
                                 /> */}
-                            </div>
-                            <div className='flex flex-col w-48'>
-                                <div className='flex flex-row justify-center items-center gap-2'>
-                                    <FaPeopleGroup />{" "}
-                                    {TheEvent.attendees &&
-                                    TheEvent.attendees.length > 0 ? (
-                                        <div className='flex flex-row'>
-                                            {TheEvent.attendees.length}{" "}
-                                            attendees
-                                        </div>
-                                    ) : (
-                                        "BE THE FIRST TO JOIN"
-                                    )}
-                                </div>
-
-                                <div
-                                    className={`cursor-pointer  flex flex-row justify-center bg-amber-500 text-lg font-medium items-center w-20 rounded-sm shadow-lg text-white self-end gap-x-3 ${
-                                        user &&
-                                        TheEvent.attendees.indexOf(user.uid) >
-                                            -1
-                                            ? "bg-gray-300 text-black"
-                                            : "hover:bg-emerald-500 transition-all duration-200"
-                                    }`}
-                                    onClick={handleAttendees}
-                                >
-                                    {user &&
-                                    TheEvent.attendees.indexOf(user.uid) > -1
-                                        ? "JOINED"
-                                        : "JOIN"}
-                                </div>
                             </div>
                         </div>
                     </div>
