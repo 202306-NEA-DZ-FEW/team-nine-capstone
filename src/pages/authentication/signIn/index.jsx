385e7884b652cdd0bc646b2b901785d829c86a32
HachemBouhadede <hachembouhadede@gmail.com>
2023-11-07 05:04:41 +0100

    feat(authentication): styling, notification of auth page, sign in providers
    style the sign in and sign up page to the according figma designe, add translation to both pages,
    add sign in with google and twitter functionality, add terms and conditions as pop up in the sign up
    page, add scroll in terms and conditions, fix redircting
    
    #67

--- src/pages/authentication/signIn/index.jsx
+++ src/pages/authentication/signIn/index.jsx
@@ -1,16 +1,32 @@
-import { signInWithEmailAndPassword } from "firebase/auth";
+import {
+    signInWithEmailAndPassword,
+    getAuth,
+    signInWithPopup,
+    GoogleAuthProvider,
+    TwitterAuthProvider,
+} from "firebase/auth";
+import Image from "next/image";
 import Link from "next/link";
 import { useRouter } from "next/router";
-import React, { useEffect } from "react";
+import { useTranslation } from "next-i18next";
+import { serverSideTranslations } from "next-i18next/serverSideTranslations";
+import React, { useEffect, useState } from "react";
+import { FaSquareXTwitter } from "react-icons/fa6";
+import { FcGoogle } from "react-icons/fc";
 
 import { auth } from "@/lib/firebase/controller";
 
 import { useUser } from "@/context/UserContext";
 import Layout from "@/layout/Layout";
 
+import background from "../../../../public/images/background.png";
+
 export default function SignUp() {
+    const [error, setError] = useState(null);
+    const [successMessage, setSuccessMessage] = useState(null);
     const { setUser } = useUser();
     const router = useRouter();
+    const { t } = useTranslation("common");
 
     async function handleSignIn(e) {
         e.preventDefault();
@@ -24,17 +40,40 @@ export default function SignUp() {
                 password
             );
             const user = userCredential.user;
-
-            // Update the user context with the signed-in user
-
-            // console.log("User Logged in ", user, email, password); //for testing
+            setSuccessMessage(t("signIn.succLog"));
             router.push("/");
-        } catch (err) {
-            const errCode = err.code;
-            const errMsg = err.message;
-            console.log(`${errMsg} and code is : ${errCode}`);
+        } catch (error) {
+            setError(t("signIn.failLog"));
         }
     }
+
+    // Function to handle Google sign-in
+    async function handleGoogleSignIn() {
+        const provider = new GoogleAuthProvider();
+
+        try {
+            await signInWithPopup(auth, provider);
+            setSuccessMessage(t("signIn.succLog"));
+            router.push("/");
+        } catch (error) {
+            setError(t("signIn.failLog"));
+        }
+    }
+
+    // Function to handle Twitter sign-in
+    async function handleTwitterSignIn() {
+        const provider = new TwitterAuthProvider();
+
+        try {
+            await signInWithPopup(auth, provider);
+            setSuccessMessage(t("signIn.succLog"));
+            router.push("/");
+        } catch (error) {
+            // Handle sign-in error
+            console.error(error);
+        }
+    }
+
     useEffect(() => {
         const Logged = auth.onAuthStateChanged((user) => {
             if (user) {
@@ -48,87 +87,116 @@ export default function SignUp() {
         };
     }, [setUser]);
 
+    useEffect(() => {
+        if (successMessage || error) {
+            // After 5 seconds, remove the success message
+            const timer = setTimeout(() => {
+                setError(null);
+                setSuccessMessage(null);
+            }, 5000);
+
+            // Clear the timer if the component unmounts or if successMessage changes
+            return () => clearTimeout(timer);
+        }
+    }, [successMessage, error]);
+
     return (
         <Layout>
-            <div className='relative flex flex-col justify-center h-screen'>
-                <div className='lg:flex lg:gap-x-4 justify-center items-center mx-4'>
-                    <div className='w-full bg-white rounded-md lg:max-w-xl'>
-                        <h1 className='text-xl mt-4 px-4 py-2 font-semibold text-center text-gray-700'>
-                            Login with:
-                        </h1>
-                        {/* // the following will be used only to login users that originally signed up with one of the providers */}
-                        <div className='mt-6'>
-                            <button
-                                // onClick={handleTwitterSignIn}
-                                className='w-full mt-4 px-4 py-2 text-white bg-gray-400 rounded-lg hover:bg-gray-500 focus:outline-none focus:bg-gray-600'
-                            >
-                                Twitter
-                            </button>
-
-                            <button
-                                // onClick={handleGoogleSignIn}
-                                className='w-full mt-4 px-4 py-2 text-white bg-red-400 rounded-lg hover:bg-red-500 focus:outline-none focus:bg-red-600'
-                            >
-                                Google
-                            </button>
-                        </div>
-                        <h1 className='text-xl mt-4 px-4 py-2 font-semibold text-center text-gray-700'>
-                            or with:
-                        </h1>
-                        <form className='mt-6' onSubmit={handleSignIn}>
-                            <div className='mb-2'>
-                                <label
-                                    htmlFor='email'
-                                    className='block text-sm font-semibold text-gray-800'
-                                >
-                                    Email
-                                </label>
-                                <input
-                                    name='email'
-                                    type='email'
-                                    className='block w-full px-4 py-2 mt-2 text-gray-700 bg-white border rounded-md focus:border-gray-400 focus:ring-gray-300 focus:outline-none focus:ring focus:ring-opacity-40'
-                                />
-                            </div>
-                            <div className='mb-2'>
-                                <label
-                                    htmlFor='password'
-                                    className='block text-sm font-semibold text-gray-800'
-                                >
-                                    Password
-                                </label>
-                                <input
-                                    name='password'
-                                    type='password'
-                                    className='block w-full px-4 py-2 mt-2 text-gray-700 bg-white border rounded-md focus:border-gray-400 focus:ring-gray-300 focus:outline-none focus:ring focus:ring-opacity-40'
-                                />
-                            </div>
-                            <Link
-                                href='./forgotPassword'
-                                className='text-xs text-gray-800 font-bold'
-                            >
-                                Forget Your <u> Password ! </u>
-                            </Link>
-                            <div className='mt-6'>
-                                <button className='w-full px-4 py-2 text-white bg-gray-800 rounded-lg hover:bg-gray-900 focus:outline-none focus:bg-gray-600'>
-                                    Sign In
-                                </button>
-                            </div>
-                        </form>
-
-                        <p className='mt-2 text-xs text-center text-gray-700'>
-                            {" "}
-                            Become a member?{" "}
-                            <Link
-                                className='font-medium text-gray-600 hover:underline'
-                                href='./signUp'
-                            >
-                                {" "}
-                                Sign Up{" "}
-                            </Link>
+            <div className='relative flex justify-center items-center h-screen'>
+                <Image src={background} alt='background' layout='fill' />
+                <div className='flex flex-col justify-center items-center content-center bg-white bg-opacity-50 rounded-lg z-30 w-2/3 py-5 space-y-4'>
+                    <form
+                        onSubmit={handleSignIn}
+                        className='flex flex-col space-y-2 w-full justify-center items-center content-center'
+                    >
+                        <input
+                            className='px-4 py-2 sm:text-2xl border-2 rounded-lg bg-black opacity-50 w-3/4 sm:w-1/2 text-white'
+                            placeholder={t("signIn.EnterYourEmail")}
+                            name='email'
+                            type='Email'
+                        />
+                        <input
+                            className='px-4 py-2 sm:text-2xl border-2 rounded-lg bg-black opacity-50 w-3/4 sm:w-1/2 text-white'
+                            placeholder={t("signIn.EnterYourPassword")}
+                            type='password'
+                            name='password'
+                            minLength='8'
+                        />
+                        <p className='sm:text-2xl font-semibold'>
+                            {t("signIn.PassReset1")}
+                            <u className='hover:font-bold'>
+                                <Link href='./forgotPassword'>
+                                    {t("signIn.PassReset2")}
+                                </Link>
+                            </u>
                         </p>
+                        <button
+                            className='rounded-lg sm:text-2xl px-4 py-2 text-center bg-amber-400 hover:bg-green-500 focus:bg-green-700'
+                            type='submit'
+                        >
+                            {t("signIn.button")}
+                        </button>
+                    </form>
+                    {error && (
+                        <div className='flex justify-center items-center'>
+                            <p className='px-4 py-2 text-red-500 border-white w-fit bg-red-300 justify-center items-center rounded-lg slide-in'>
+                                {error}
+                            </p>
+                        </div>
+                    )}
+                    {successMessage && (
+                        <div className='flex justify-center items-center'>
+                            <p className='px-4 py-2 text-green-700 border-white bg-green-200 justify-center items-center w-fit rounded-lg slide-in'>
+                                {successMessage}
+                            </p>
+                        </div>
+                    )}
+                    <div className='sm:text-2xl font-semibold justify-center items-center'>
+                        {t("signIn.noAccount")}
+                        <u className='hover:font-bold'>
+                            {" "}
+                            <Link href='./signUp'>
+                                {" "}
+                                {t("signIn.noAccount2")}{" "}
+                            </Link>
+                        </u>
+                    </div>
+                    <span className='flex justify-center items-center w-4/5 mx-5 text-2xl'>
+                        <div className='grow shrink basis-0 h-0.5 bg-stone-900' />
+                        <h1 className='font-bold mx-4'>{t("signIn.or")}</h1>
+                        <div className='grow shrink basis-0 h-0.5 bg-stone-900' />
+                    </span>
+                    <div className='flex flex-col space-y-4'>
+                        <button
+                            onClick={handleGoogleSignIn}
+                            className='px-4 py-2 text-black bg-white border-trnasparent rounded-lg hover:bg-black hover:text-white focus:outline-none focus:bg-black'
+                        >
+                            <div className='flex flex-row sm:text-xl'>
+                                <FcGoogle size={24} className='mx-2' />{" "}
+                                {t("signIn.Google")}
+                            </div>
+                        </button>
+                        <button
+                            onClick={handleTwitterSignIn}
+                            className='px-4 py-2 text-black bg-white border-trnasparent opacity-100 rounded-lg hover:bg-sky-500 focus:outline-none focus:bg-sky-600'
+                        >
+                            <div className='flex flex-row sm:text-xl'>
+                                <FaSquareXTwitter size={24} className='mx-2' />{" "}
+                                {t("signIn.Twitter")}
+                            </div>
+                        </button>
                     </div>
                 </div>
             </div>
         </Layout>
     );
+}
+
+export async function getStaticProps({ locale }) {
+    return {
+        props: {
+            ...(await serverSideTranslations(locale, ["common"])),
+            // Will be passed to the page component as props
+        },
+    };
 }
