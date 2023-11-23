ef8ff5f712a46dddb4b3c92bae2408191aa50e58
imane omari <imane3.omari@gmail.com>
2023-11-23 20:59:16 +0100

    fix(edit profile & profile page): enhance styling of edit profile & profile page

--- src/pages/profile/[id].jsx
+++ src/pages/profile/[id].jsx
@@ -66,93 +66,152 @@ function Profile() {
         <Layout>
             {userData && (
                 <div className='flex flex-col '>
-                    {/* first profile section*/}
-                    <div className='relative py-5 bg-green-700 bg-opacity-50 w-full '>
-                        <div className='flex flex-col sm:flex-row group justify-center items-center align-middle space-x-10 z-10 '>
-                            {userData.avatar ? (
-                                <Image
-                                    src={userData.avatar}
-                                    alt='Avatar'
-                                    className='rounded-full border-orange-400 border-2 w-24 h-24 lg:w-32 lg:h-32 xl:w-40 xl:h-40'
-                                    width={120}
-                                    height={120}
-                                />
-                            ) : (
-                                <Image
-                                    src='/images/defaultUser.png'
-                                    alt='Default'
-                                    className='rounded-full border-orange-400 border-2 w-24 h-24 lg:w-32 lg:h-32 xl:w-40 xl:h-40'
-                                    width={120}
-                                    height={120}
-                                />
-                            )}
-                            <div className='w-3/4 sm:w-1/2 justify-center text-center '>
-                                <h1 className=' flex justify-around items-center text-3xl font-bold mt-3 p-2 rounded-t-lg bg-amber-400'>
-                                    {userData.displayName}
+                    <div className='flex flex-col sm:flex-row justify-center items-center mx-10'>
+                        {/* first profile section*/}
+                        <div className=' md:w-1/4  sm:w-1/2 mx-10 mt-10 bg-yellow-500 w-full'>
+                            <div className='flex justify-center mt-10'>
+                                {userData.avatar ? (
+                                    <div className='flex flex-col items-center'>
+                                        <Image
+                                            src={userData.avatar}
+                                            alt='Avatar'
+                                            className='rounded-full border-yellow-400 border-8 w-30 h-30 lg:w-32 lg:h-32 xl:w-40 xl:h-40'
+                                            width={120}
+                                            height={120}
+                                        />
+                                        <div className='w-3/4 sm:w-1/2 justify-center text-center  '>
+                                            <h1 className='flex justify-around items-center text-3xl font-normal text-white p-2'>
+                                                {userData.displayName}
+                                            </h1>
+                                        </div>
+                                        <div className='w-3/4 sm:w-1/2 justify-center text-center  '>
+                                            <h1 className='flex justify-around items-center text-3xl font-normal text-white p-2'>
+                                                {userData.currentEmail}
+                                            </h1>
+                                        </div>
+                                        <div className='flex justify-center text-center bg-white hover:bg-yellow-400 mt-16'>
+                                            <h1 className='flex items-center text-xl font-normal text-gray-400 '>
+                                                {isOwner && (
+                                                    <Link
+                                                        href={`/profile/${id}/editProfile`}
+                                                    >
+                                                        <div className='flex justify-center items-center ml-2 w-72 bg-white'>
+                                                            <span>
+                                                                {t(
+                                                                    "profile.ep"
+                                                                )}
+                                                            </span>
+                                                            <LiaUserEditSolid className='ml-2 hover:rounded-full hover:bg-black hover:text-white' />
+                                                        </div>
+                                                    </Link>
+                                                )}
+                                            </h1>
+                                        </div>
+                                    </div>
+                                ) : (
+                                    <div className='flex flex-col items-center'>
+                                        <Image
+                                            src='/images/defaultUser.png'
+                                            alt='Default'
+                                            className='rounded-full border-yellow-400 border-8 w-30 h-30 lg:w-32 lg:h-32 xl:w-40 xl:h-40'
+                                            width={120}
+                                            height={120}
+                                        />
+                                        <div className='w-3/4 sm:w-1/2 justify-center text-center mt-3'>
+                                            <h1 className='flex justify-around items-center text-3xl font-bold p-2 rounded-t-lg bg-amber-400'>
+                                                {userData.displayName}
+                                                {isOwner && (
+                                                    <Link
+                                                        href={`/profile/${id}/editProfile`}
+                                                    >
+                                                        <LiaUserEditSolid className='justify-end hover:rounded-full hover:bg-black hover:text-white' />
+                                                    </Link>
+                                                )}
+                                            </h1>
+                                        </div>
+                                        <div className='w-3/4 sm:w-1/2 justify-center text-center mt-3'>
+                                            <h1 className='flex justify-around items-center text-3xl font-bold p-2 rounded-t-lg bg-amber-400'>
+                                                {userData.currentEmail}
+                                                {isOwner && (
+                                                    <Link
+                                                        href={`/profile/${id}/editProfile`}
+                                                    >
+                                                        <LiaUserEditSolid className='justify-end hover:rounded-full hover:bg-black hover:text-white' />
+                                                    </Link>
+                                                )}
+                                            </h1>
+                                        </div>
+                                    </div>
+                                )}
+                            </div>
+                        </div>
 
-                                    {isOwner && (
-                                        <Link
-                                            href={`/profile/${id}/editProfile`}
-                                        >
-                                            <LiaUserEditSolid className='justify-end hover:rounded-full hover:bg-black hover:text-white' />
-                                        </Link>
-                                    )}
+                        {/* second profile section */}
+                        <div className='grid justify-center bg-white mt-10 md:w-3/4'>
+                            <div className='flex h-24 m-2 bg-yellow-400 items-center'>
+                                <h1 className='font-semibold text-2xl ml-10'>
+                                    {t("profile.pro")}
                                 </h1>
                             </div>
-                        </div>
-                    </div>
-
-                    {/* second profile section */}
-                    <div>
-                        <div className='flex p-2 ml-10 sm:text-lg sm:flex-row flex-col justify-start items-start my-3'>
-                            <div className='w-48'>{t("profile.Full Name")}</div>{" "}
-                            <div className='font-semibold '>
-                                {userData.fullName}
+                            <div className='flex flex-col p-2 sm:text-lg sm:flex-row my-3'>
+                                <div className='w-full sm:w-1/3 font-semibold'>
+                                    {t("profile.Full Name")}
+                                </div>
+                                <div className='w-full sm:w-2/3 mt-2 sm:mt-0'>
+                                    {userData.fullName}
+                                </div>
+                            </div>
+                            <div className='flex flex-col p-2 sm:text-lg sm:flex-row my-3'>
+                                <div className='w-full sm:w-1/3 font-semibold'>
+                                    {t("profile.Location")}
+                                </div>
+                                <div className='w-full sm:w-2/3 mt-2 sm:mt-0'>
+                                    {userData.location}
+                                </div>
+                            </div>
+                            <div className='flex flex-col p-2 sm:text-lg sm:flex-row justify-start items-start my-3 font-semibold'>
+                                <div className='w-full sm:w-1/3'>
+                                    {t("profile.Interests")}
+                                </div>
+                                {userData.userInterests &&
+                                userData.userInterests.length > 0 ? (
+                                    <div className='flex flex-wrap items-center justify-center mt-2'>
+                                        {userData.userInterests.map(
+                                            (interest) => (
+                                                <div
+                                                    key={interest}
+                                                    className='flex flex-col sm:flex-row w-full sm:w-40 h-14 p-2 m-2 items-center text-center justify-center border-transparent rounded-lg bg-green-500 hover:bg-green-700 hover:text-white'
+                                                >
+                                                    {interest}
+                                                </div>
+                                            )
+                                        )}
+                                    </div>
+                                ) : (
+                                    <div className='w-full sm:w-2/3 mt-2 sm:mt-0'>
+                                        {isOwner ? (
+                                            <div>
+                                                {t("profile.noIntrests")}{" "}
+                                                <Link
+                                                    className='text-blue-700 underline hover:font-semibold hover:rounded-md hover:bg-yellow-400 px-2'
+                                                    href={`/profile/${id}/editProfile`}
+                                                >
+                                                    {t("profile.ep")}
+                                                </Link>{" "}
+                                                {t("profile.toaddsome")}
+                                            </div>
+                                        ) : (
+                                            <div>
+                                                {t(
+                                                    "profile.No interests found."
+                                                )}
+                                            </div>
+                                        )}
+                                    </div>
+                                )}
                             </div>
                         </div>
-                        <div className='flex p-2 ml-10 sm:text-lg sm:flex-row flex-col justify-start items-start my-3'>
-                            <div className='w-48'>{t("profile.Location")}</div>
-                            <div className='font-semibold '>
-                                {userData.location}
-                            </div>
-                        </div>
-                        <div className='flex flex-col items-center justify-center text-lg font-semibold'>
-                            {t("profile.Interests")}{" "}
-                            {userData.userInterests &&
-                            userData.userInterests.length > 0 ? (
-                                <div className='flex flex-wrap items-center justify-center'>
-                                    {userData.userInterests.map((interest) => (
-                                        <div
-                                            key={interest}
-                                            className='flex flex-col sm:flex-row w-40 h-14 p-2 m-2 items-center text-center justify-center border-transparent rounded-lg bg-green-500 hover:bg-green-700 hover:text-white'
-                                        >
-                                            {interest}
-                                        </div>
-                                    ))}
-                                </div>
-                            ) : (
-                                <div>
-                                    {isOwner ? (
-                                        <div>
-                                            {t("profile.noIntrests")}{" "}
-                                            <Link
-                                                className='text-indigo-800 underline hover:font-semibold hover:rounded-lg hover:bg-orange-300'
-                                                href={`/profile/${id}/editProfile`}
-                                            >
-                                                {t("profile.ep")}
-                                            </Link>{" "}
-                                            {t("profile.toaddsome")}
-                                        </div>
-                                    ) : (
-                                        <div>
-                                            {t("profile.No interests found.")}
-                                        </div>
-                                    )}
-                                </div>
-                            )}
-                        </div>
                     </div>
-
                     <div className='grow shrink m-2 h-0.5 bg-stone-900' />
                     {/* divider */}
 
@@ -183,14 +242,14 @@ function Profile() {
                                 <div>
                                     <Link
                                         href='/createTheEvent'
-                                        className='text-indigo-800 underline hover:font-semibold hover:rounded-lg hover:bg-orange-300'
+                                        className='text-blue-700 underline hover:font-semibold hover:rounded-md hover:bg-yellow-400 px-2'
                                     >
                                         {t("createEvent.title")}{" "}
                                     </Link>
                                     {t("profile.orCheck")}{" "}
                                     <Link
                                         href='/events'
-                                        className='text-indigo-800 underline hover:font-semibold hover:rounded-lg hover:bg-orange-300'
+                                        className='text-blue-700 underline hover:font-semibold hover:rounded-md hover:bg-yellow-400 px-2'
                                     >
                                         {t("Events")}
                                     </Link>
